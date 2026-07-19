adc144e2370ca1bea25e86f021d3082dba931d65
Posa Karthik <posakarthik16@gmail.com>
2026-07-20 03:23:24 +0530

    feat(arrays): added DSA #001 largest element solution

--- src/main/java/com/nexorithmm/dsa/arrays/LargestElement.java
+++ src/main/java/com/nexorithmm/dsa/arrays/LargestElement.java
@@ -3,6 +3,72 @@ package com.nexorithmm.dsa.arrays;
 public class LargestElement {
     public static void main(String[] args) {
 
-        
+        /*
+
+        📝 Problem Statement
+
+        Given an integer array, find the largest element.
+
+                Example
+        Input:
+    [5, 2, 9, 1, 7]
+
+        Output:
+        9
+
+        🧪 Test Cases
+            Input:
+         [5, 2, 9, 1, 7]
+
+            Output:
+                9
+            Input:
+            [10]
+
+            Output:
+                10
+               Input:
+            [-5, -2, -8]
+
+            Output:
+                 -2
+                Input:
+            [7, 7, 7]
+
+            Output:
+                7
+
+        📌 Constraints
+            Array size ≥ 1
+            Array may contain negative numbers
+            Array may contain duplicate values
+
+            📚 Time Complexity
+                        O(n)
+
+            We visit each element once.
+
+                📚 Space Complexity
+                         O(1)
+
+We only use one extra variable (largestElement), regardless of the array size.
+
+         */
+
+
+        int[] array = {5, 2, 9, 1, 7};
+
+        int largestElement = array[0];
+
+        for(int i = 1;i < array.length; i++){
+
+            if(array[i] > largestElement){
+                largestElement = array[i];
+            }
+
+        }
+
+        System.out.println("Largest Element : "+largestElement);
+
     }
 }
